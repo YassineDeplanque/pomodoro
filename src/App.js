9b0f5aacd15d6a352ba88ed1cf08d71e81c37783
unknown <yassine59958@gmail.com>
2025-08-11 23:23:19 +0200

    handle bigger rest

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 function App() {
 
-const [seconde, setSeconde] = useState(59);
+const [seconde, setSeconde] = useState(0);
 const [minute, setMinute] = useState(25);
 const [work, setWork] = useState(true);
 const [cycle, setCycle] = useState(1);
@@ -14,13 +14,17 @@ useEffect(() => {
       setMinute(minute - 1);
       setSeconde(59);
     }
-    if(seconde == 0 && minute == 0 && work == true){
+    if(seconde == 0 && minute == 0 && work == true && cycle%4 == 0){
+      setMinute(15);
+      setSeconde(0);
+      setWork(false)
+    }else if(seconde == 0 && minute == 0 && work == true && cycle%4 !== 0){
       setMinute(5);
-      setSeconde(59);
+      setSeconde(0);
       setWork(false)
     } else if(seconde == 0 && minute == 0 && work == false) {
       setMinute(25);
-      setSeconde(59);
+      setSeconde(0);
       setWork(true)
       setCycle(cycle + 1);
     }
@@ -32,6 +36,7 @@ useEffect(() => {
     <div>
       <h1>{minute} : {seconde}</h1>
       <h3>{work ? 'Working time.' : 'Rest time !'}</h3>
+      <h3>{cycle%4==0 && work==false ? 'Take a bigger rest.' : "Don't give up !"}</h3>
       <h3>Cycle number {cycle}</h3>
     </div>
   );
